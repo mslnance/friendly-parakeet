c0ea9f237c0152f7ae13f0814d336fa7715b3241
Symphony <55611595+lnance6@users.noreply.github.com>
2020-08-23 21:30:06 -0700

    Prompt for and display of character type

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,8 +4,7 @@
 //WHEN prompted for password criteria
 //THEN I select which criteria to include in the password
 
-//WHEN prompted for character types to include in the password
-//THEN I choose lowercase, uppercase, numeric, and/or special characters
+
 //WHEN I answer each prompt
 //THEN my input should be validated and at least one character type should be selected
 //WHEN all prompts are answered
@@ -13,14 +12,53 @@
 //WHEN the password is generated
 //THEN the password is either displayed in an alert or written to the page
 
+//WHEN prompted for character types to include in the password
+//THEN I choose lowercase, uppercase, numeric, and/or special characters
+var pwValues = "";
+var charTypes=function() {
+  var pwLtrValues = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  var pwNbrValues = "123456780";
+  var pwSymValues= "!#$%&'()*+,-./:;<=>?@[\]^_`{|}~";
+  
+  var confirmUC = window.confirm("Should it include uppercase letters?");
+  if (confirmUC) {
+    pwValues = pwLtrValues;
+    console.log(pwValues);
+  }
+  //pwValues = pwValues;
+
+  var confirmLC = window.confirm("Should it include lowercase letters?");
+  if (confirmLC) {
+    pwValues = pwValues + pwLtrValues.toLowerCase();
+    console.log(pwValues);
+  }  
+  //pwValues = pwValues;
+
+  var confirmNbr = window.confirm("Should it include numbers?");
+  if (confirmNbr) {
+    pwValues = pwValues + pwNbrValues;
+    console.log(pwValues);
+  }
+  //pwValues = pwValues;
+
+  var confirmSym = window.confirm("Should it include special characters?");
+  if (confirmSym) {
+    pwValues = pwValues + pwSymValues;
+    console.log(pwValues);
+  }
+  //pwValues = pwValues;
+}
+
 var generatePassword=function() {
 
-  var pwValues = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz123456780!#$%&'()*+,-./:;<=>?@[\]^_`{|}~"
+  
   //WHEN prompted for the length of the password
   //THEN I choose a length of at least 8 characters and no more than 128 characters
   var password = "";
   var promptPwLength = window.prompt('Please choose a password length between 8 and 128 characters');
   if (promptPwLength >= 8 && promptPwLength <= 128) {
+    charTypes();
+    console.log(pwValues);
     for(var i=0; i < promptPwLength; i++) {
         password = password + pwValues.charAt(Math.floor(Math.random() * Math.floor(pwValues.length - 1)));
         
